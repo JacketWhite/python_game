feb8b564166b8b3e0cd7ebbcb7f273149cb63148
Dávid Fehér <feherkeee@gmail.com>
2016-05-09 15:48:12 +0200

    Listing Task and Archive completed

--- to_do_app.py
+++ to_do_app.py
@@ -1,9 +1,21 @@
 def Listing():
+    print ("You saved the following to-do items:")
     listing = open('TaskList.md', 'r')
     text_in_file = listing.readlines()
     listing.close()
     lines = 1
     for i in text_in_file:
-        print(str(lines) + '. [ ] ' + i, end = "")
+        print(str(lines) + i, end = "")
         lines += 1
+
 Listing()
+
+def Archive():
+    print("All completed tasks got deleted")
+    with open("TaskList.md", "r+") as f:
+        text_in_file = f.readlines()
+        f.seek(0)
+        for line in text_in_file:
+            if "[x]" not in line:
+                f.write(line)
+        f.truncate()
